5d929224eaad6cdcc2b50d1bf48f8e66eba3e3e9
u32luke <u32Luke@gmail.com>
2022-04-23 19:34:53 -0400

    Inital commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use mini_redis::{Connection, Frame};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    // Bind the listener to the address
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        // A new task is spawned for each inbound socket. The socket is
+        // moved to the new task and processed there.
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream) {
+    // The `Connection` lets us read/write redis **frames** instead of byte streams. The `Connection` type is defined by mini-redis.
+    let mut connection = Connection::new(socket);
+
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("Got: {:?}", frame);
+
+        // Respond with an error
+        let response = Frame::Error("unimplemented".to_string());
+        connection.write_frame(&response).await.unwrap();
+    }
+}
